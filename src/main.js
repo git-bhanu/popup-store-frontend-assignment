84565014e19ddee4abc8fa5663871989ebdb8075
Bhanu <bhanu@krenovate.com>
2022-03-13 02:18:38 +0530

    Dropzone area working

--- src/main.js
+++ src/main.js
@@ -3,14 +3,39 @@ import App from './App.vue'
 import router from './router'
 import store from './store/index.js';
 
-
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faPlus, faBolt, faEllipsis, faTag, faEnvelopeOpen, faMagnifyingGlass, faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+import { 
+    faPlus, 
+    faBolt, 
+    faEllipsis, 
+    faTag, 
+    faEnvelopeOpen, 
+    faMagnifyingGlass, 
+    faAngleLeft, 
+    faLock,
+    faFileInvoice,
+    faUserTag,
+    faTags,
+    faPaperPlane
+} from "@fortawesome/free-solid-svg-icons";
 
-library.add(faPlus, faBolt, faEllipsis, faTag, faEnvelopeOpen, faMagnifyingGlass, faAngleLeft);
+library.add(
+    faPlus, 
+    faBolt, 
+    faEllipsis, 
+    faTag, 
+    faEnvelopeOpen, 
+    faMagnifyingGlass, 
+    faAngleLeft, 
+    faLock,
+    faFileInvoice,
+    faUserTag,
+    faTags,
+    faPaperPlane
+    );
 
 
 const app = createApp(App)
